a7ec76f54c3a1c873f7802888b3253c3ddf5523d
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2024-10-20 15:29:00 +0300

    homeworck 5.1a

--- main.py
+++ main.py
@@ -26,39 +26,30 @@ import keyword
 test_data = ['_', '__', '___', 'x', 'get_value', 'get value', 'get!value', 'some_super_puper_value', 'Get_value',
              'get_Value', '3m', 'm3', 'assert', 'assert_exception', 'some_super_puper__value']
 
+
 for test_variable in test_data:
-    print(len(test_variable))
     if len(test_variable) > 0:
-        if test_variable in keyword.kwlist:
-            print(f"Error! Found {test_variable} 1111is keyword!") # добавил 111 чтобы понимать какая срабатывает, я потом уберу
-        elif not test_variable[0].isnumeric() and test_variable.count(" ") == 0:# если сюда добавить  and test_variable.islower(), "_" не проходит эту проверку
+        if test_variable in keyword.kwlist:  # проверка ключевых слов
+            print(f"Error! Found {test_variable} is keyword!")
+        elif  not test_variable[0].isnumeric() and test_variable.islower() and test_variable.count(" ") == 0: # проверка, первый символ не должен содержать цифр, без заглавных букв и без пробелов
             is_correct = True
-            for symbol in string.punctuation.replace("_", ""):
+            for symbol in string.punctuation.replace("_", ""):  # удаляем все "_" что бы исключить "_" из проверки
                 if symbol in test_variable:
                     is_correct = False
-                    print(f"Error! Found {test_variable} 2222in variable name!")
+                    print(f"Error! Found {test_variable} in variable name!")
                     break
-        start_index = 0 # цикл работает только если подряд идут нижние подчеркиввния, остальных случаях он будет безконечным,
-        while start_index < len(test_variable) - 1 :
-            first_underscore_index = test_variable.find("_")
-            if first_underscore_index != -1 and len(test_variable) >= 2:
-                second_underscore_index = test_variable.find("_", first_underscore_index + 1)
-                if second_underscore_index != -1 and second_underscore_index - first_underscore_index == 1:
-                    is_correct = False
-                    print(f"Error! Found double '_' in {test_variable} 33333variable name!")
-                    break
-                else:
-                    start_index = first_underscore_index + 1
-            else:
-                start_index = first_underscore_index + 1
 
-        if is_correct:  # эта переменная почемуто не связана с остальными is_correct
+            first_underscore_index = test_variable.find("__")  # поиск дублей "__"
+            if first_underscore_index != -1:
+                    is_correct = False
+                    print(f"Error! Found double '_' in {test_variable} variable name!")
+
+            if is_correct:
                 print(f"Keyword {test_variable} is correct!")
         else:
-            print(f"Error! Found {test_variable} in 4444variable name!")
+            print(f"Error! Found {test_variable} in 111variable name!")
     else:
         print("Incorrect variable length!")
-
 
 # import string
 
